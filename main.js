f854d3f302c18302b99cfd2842681548e010ed21
X-Engineer-001 <fabin1790@gmail.com>
2016-07-21 18:38:53 +0800

    keapdealwith350

--- main.js
+++ main.js
@@ -303,8 +303,8 @@ function draw(){
       var thisblade=blade[editingbladepoint.blade];
       if(GetDistance(cursor.x,cursor.y,350,350)>350){
         var unitVector=GetUnitVector(350,350,cursor.x,cursor.y);
-        thisblade[editingbladepoint.x]=unitVector.x*350-350;
-        thisblade[editingbladepoint.y]=unitVector.y*350-350;
+        thisblade[editingbladepoint.x]=unitVector.x*350;
+        thisblade[editingbladepoint.y]=unitVector.y*350;
       }else{
         thisblade[editingbladepoint.x]=cursor.x-350;
         thisblade[editingbladepoint.y]=cursor.y-350;
