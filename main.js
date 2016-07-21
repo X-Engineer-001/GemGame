3e1fb588cb17ec863c0b475ca8be1914180ee140
X-Engineer-001 <fabin1790@gmail.com>
2016-07-21 19:23:31 +0800

    dealwith／

--- main.js
+++ main.js
@@ -291,7 +291,7 @@ function WriteFraction(numerator,denominator,x,y,font,color){
   ctx.font=font/String(numerator).length/2+"px Arial";
   ctx.fillText(denominator,x+font/2,y+font);
   ctx.font=font+"px Arial";
-  ctx.fillText("／",x*3/4,(y+font)*3/4);
+  ctx.fillText("/",x*3/4,(y+font)*4/3);
 }
 function draw(){
   ctx.drawImage(bg,0,0,750,700);
